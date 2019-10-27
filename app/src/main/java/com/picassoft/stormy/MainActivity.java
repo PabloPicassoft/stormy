01ef1a733ed4d6d19a256a0532da898973dd088d
Paul Iudean <pablo.picassoft@gmail.com>
2019-10-27 20:27:33 +0000

    Implemented api response functionality with hard coded request

--- app/src/main/java/com/picassoft/stormy/MainActivity.java
+++ app/src/main/java/com/picassoft/stormy/MainActivity.java
@@ -1,14 +1,60 @@
 package com.picassoft.stormy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "deb73aaa08247702ac907c35733096bb";
+        double latitiude = 37.8267;
+        double longitude = -122.4233;
+
+        String forecastURL = "https://api.darksky.net/forecast/"
+                + apiKey + "/"+ latitiude +"," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(forecastURL)
+                .build();
+
+        Call call = client.newCall(request);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    } else {
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "IO Exception Caught");
+                }
+            }
+        });
+        Log.d(TAG, "Main UI Code is Running, HOORAY!");
     }
 }
